3b8732f352a9953bf74a43391f7f3b6e08a6f3c0
sussygamedeveloper <98538512+sussygamedeveloper@users.noreply.github.com>
2022-03-15 08:59:03 -0400

    Update ubuntu_minecraft_launcher.sh

--- ubuntu_minecraft_launcher.sh
+++ ubuntu_minecraft_launcher.sh
@@ -3,6 +3,7 @@ apt update
 apt install -y xvfb x11vnc build-essential libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libgl1-mesa-dev libgl1-mesa-dri
 apt install -y openjdk-8-jre
 git clone https://github.com/sussygamedeveloper/noVNC
+chmod +x noVNC/utils/launch.sh
 ./noVNC/utils/launch.sh --listen 80 &
 sleep 3
 apt install -y tigervnc-standalone-server twm qtbase5-dev x11-utils xdotool
