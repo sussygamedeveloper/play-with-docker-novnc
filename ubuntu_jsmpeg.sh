1b3fbf13ce0365e991ca3cf6a2948086ee857152
What? <98538512+sussygamedeveloper@users.noreply.github.com>
2022-05-09 09:21:54 -0400

    Update ubuntu_jsmpeg.sh

--- ubuntu_jsmpeg.sh
+++ ubuntu_jsmpeg.sh
@@ -3,6 +3,6 @@ apt update
 apt install -y xvfb fluxbox xterm
 wget https://github.com/sussygamedeveloper/play-with-docker-novnc/raw/main/jsmpeg-vnc-linux_FIX_FOR_FAKEPORT.zip
 unzip jsmpeg-vnc-linux_FIX_FOR_FAKEPORT.zip
-Xvfb :0 -screen 1 1280x1024x24 &
+Xvfb :1 -screen 1 1280x1024x24 &
 ./jsmpeg-vnc -display_number 1 -port 80 &
 fluxbox &
